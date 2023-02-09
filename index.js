4ecfc3643886749906645eaae170ccbc05667fda
elfaraji <mohmedfaraji5@gmail.com>
2023-02-09 14:38:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,237 @@
+import express from 'express';
+const app = express()
+
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+import { readFile } from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+mongoose.set("strictQuery", false);
+try {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log("connected to the database");
+} catch (error) {
+  console.log("impossible to connect to the database" + error);
+
+}
+
+var Schema = mongoose.Schema;
+
+var exerciseUsersSchema = new Schema({
+  username: { type: String, unique: true, required: true }
+});
+
+var ExerciseUsers = mongoose.model('ExerciseUsers', exerciseUsersSchema);
+
+var exercisesSchema = new Schema({
+  userId: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, min: 1, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+var Exercises = mongoose.model('Exercises', exercisesSchema);
+
+app.post('/api/users', (req, res) => {
+  if (!req.body.username) {
+    res.send("Path `username` is required.");
+  }
+  let username = req.body.username;
+  let _id = '';
+  ExerciseUsers.findOne({ "username": username }, function (err, data) {
+    if (!err && data === null) {
+      let user = new ExerciseUsers({
+        username: username
+      });
+
+      user.save((err, data) => {
+        if (err) {
+          console.log("error in save user");
+        }
+        else {
+          _id = data['_id'];
+
+          return res.json({
+            _id: _id,
+            username: username
+          });
+        }
+      });
+    }
+    else {
+      return res.send("username already taken");
+    }
+  });
+});
+
+app.get('/api/users', (req, res) => {
+  const users = [];
+  ExerciseUsers.find({}, function (err, data) {
+    if (!err) {
+      data.forEach((user) => {
+        users.push({
+          _id: user['_id'],
+          username: user['username']
+        });
+      });
+      console.log(users);
+      return res.json(users);
+    }
+   else
+      return res.send("error");
+  });
+});
+
+app.post('/api/users/:_id/exercises', function (req, res) {
+	if (req.params._id === '0') {
+		return res.json({ error: '_id is required' });
+	}
+
+	if (req.body.description === '') {
+		return res.json({ error: 'description is required' });
+	}
+
+	if (req.body.duration === '') {
+		return res.json({ error: 'duration is required' });
+	}
+
+	let userId = req.params._id;
+	let description = req.body.description;
+	let duration = parseInt(req.body.duration);
+	let date = (req.body.date !== undefined ? new Date(req.body.date) : new Date());
+
+	if (isNaN(duration)) {
+		return res.json({ error: 'duration is not a number' });
+	}
+
+	if (date == 'Invalid Date') {
+		return res.json({ error: 'date is invalid' });
+	}
+
+	ExerciseUsers.findById(userId, function (err, data) {
+		if (!err && data !== null) {
+			let newExercise = new Exercises({
+				userId: userId,
+				description: description,
+				duration: duration,
+				date: date
+			});
+
+			newExercise.save(function (err2, data2) {
+				if (!err2) {
+					return res.json({
+						_id: data['_id'],
+						username: data['username'],
+						description: data2['description'],
+						duration: data2['duration'],
+						date: new Date(data2['date']).toDateString()
+					});
+				}
+			});
+		} else {
+			return res.json({ error: 'user not found' });
+		}
+	});
+});
+
+app.get('/api/users/:_id/exercises', function (req, res) {
+	res.redirect('/api/users/' + req.params._id + '/logs');
+});
+app.get('/api/users/:_id/logs', function (req, res) {
+	let userId = req.params._id;
+	let findConditions = { userId: userId };
+
+	if (
+		(req.query.from !== undefined && req.query.from !== '')
+		||
+		(req.query.to !== undefined && req.query.to !== '')
+	) {
+		findConditions.date = {};
+
+		if (req.query.from !== undefined && req.query.from !== '') {
+			findConditions.date.$gte = new Date(req.query.from);
+		}
+
+		if (findConditions.date.$gte == 'Invalid Date') {
+			return res.json({ error: 'from date is invalid' });
+		}
+
+		if (req.query.to !== undefined && req.query.to !== '') {
+			findConditions.date.$lte = new Date(req.query.to);
+		}
+
+		if (findConditions.date.$lte == 'Invalid Date') {
+			return res.json({ error: 'to date is invalid' });
+		}
+	}
+
+	let limit = (req.query.limit !== undefined ? parseInt(req.query.limit) : 0);
+
+	if (isNaN(limit)) {
+		return res.json({ error: 'limit is not a number' });
+	}
+
+	ExerciseUsers.findById(userId, function (err, data) {
+		if (!err && data !== null) {
+			Exercises.find(findConditions).sort({ date: 'asc' }).limit(limit).exec(function (err2, data2) {
+				if (!err2) {
+					return res.json({
+						_id: data['_id'],
+						username: data['username'],
+						log: data2.map(function (e) {
+							return {
+								description: e.description,
+								duration: e.duration,
+								date: new Date(e.date).toDateString()
+							};
+						}),
+						count: data2.length
+					});
+				}
+			});
+		} else {
+			return res.json({ error: 'user not found' });
+		}
+	});
+});
+
+app.use((req, res, next) => {
+	return next({ status: 404, message: 'not found' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+	let errCode, errMessage;
+
+	if (err.errors) {
+		// mongoose validation error
+		errCode = 400; // bad request
+		const keys = Object.keys(err.errors);
+		// report the first validation error
+		errMessage = err.errors[keys[0]].message;
+	} else {
+		// generic or custom error
+		errCode = err.status || 500;
+		errMessage = err.message || 'Internal Server Error';
+	}
+
+	res.status(errCode).type('txt')
+		.send(errMessage);
+});
+  const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log('Your app is listening on port ' + listener.address().port)
+  })
